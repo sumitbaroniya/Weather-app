6bbef818b69c7db669ac70e1edbb8bf63d22cd82
sumitbaroniya <75536160+sumitbaroniya@users.noreply.github.com>
2022-08-15 11:47:54 +0530

    Update weatherService.js

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,6 +1,6 @@
 import { DateTime } from "luxon";
 
-const API_KEY = "";
+const API_KEY = "f60fe4356139d52dd53d7973d8f1549d";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 // https://api.openweathermap.org/data/2.5/onecall?lat=48.8534&lon=2.3488&exclude=current,minutely,hourly,alerts&appid=1fa9ff4126d95b8db54f3897a208e91c&units=metric
